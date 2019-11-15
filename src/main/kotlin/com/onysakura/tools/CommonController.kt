b4df41bd284b51cc9ab1e9da02d9e99aad9e94de
guojinpeng <gjp0609@qq.com>
2019-11-15 17:58:01 +0800

    edit

--- src/main/kotlin/com/onysakura/tools/CommonController.kt
+++ src/main/kotlin/com/onysakura/tools/CommonController.kt
@@ -1,14 +1,12 @@
 package com.onysakura.tools;
 
-import org.springframework.scheduling.annotation.EnableScheduling
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 @RequestMapping("/page")
-@EnableScheduling
 class CommonController {
 
     @GetMapping("/{page}")
