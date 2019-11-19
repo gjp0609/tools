c51a6d50117b8a8c553eef7abc935f645663f6fb
guojinpeng <gjp0609@qq.com>
2019-11-19 17:34:48 +0800

    add search branch

--- src/main/kotlin/com/onysakura/tools/qrcode/QrCode.kt
+++
@@ -1,7 +0,0 @@
-package com.onysakura.tools.qrcode
-
-data class QrCode(val url: String, val code: String) {
-    override fun toString(): String {
-        return "QrCode(url='$url', code='$code')"
-    }
-}
--- src/main/kotlin/com/onysakura/tools/url/UrlRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.onysakura.tools.url
-
-import org.springframework.data.jpa.repository.JpaRepository
-import java.util.*
-
-interface UrlRepository : JpaRepository<UrlEntity, String> {
-    fun findByUrl(url: String): Optional<UrlEntity>
-}
