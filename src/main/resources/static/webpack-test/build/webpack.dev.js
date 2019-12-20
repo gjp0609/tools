43e3c4e979d0a1d6547e4f52db4eb6d5ec28e9ec
guojinpeng <gjp0609@qq.com>
2019-12-20 17:11:21 +0800

    test webpack

---
+++ src/main/resources/static/webpack-test/build/webpack.dev.js
@@ -0,0 +1,16 @@
+const common = require('./webpack.base.js');
+const merge = require('webpack-merge');
+const path = require('path');
+
+module.exports = merge(common, {
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: '../dist'
+    },
+    output: {
+        filename: 'js/[name].[hash].js', // 每次保存 hash 都变化
+        path: path.resolve(__dirname, '../dist')
+    },
+    module: {},
+    mode: 'development',
+});
---
+++ src/main/resources/static/webpack-test/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="index">
+        <span>{{test}}</span>
+        <el-button @click="asd">qwe</el-button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "App",
+        data() {
+            return {
+                test: 1
+            }
+        },
+        methods: {
+            asd() {
+                alert(123)
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .index {
+        span {
+            color: #00c4a7;
+        }
+    }
+</style>
