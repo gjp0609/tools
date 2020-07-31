5738210b00fe5fbeb65d98926267fa928b02ee6f
guojinpeng <gjp0609@qq.com>
2020-07-31 14:58:05 +0800

    add cors

--- src/main/kotlin/com/onysakura/tools/test/TestController.kt
+++ src/main/kotlin/com/onysakura/tools/test/TestController.kt
@@ -5,9 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-
 @RestController
-@CrossOrigin(origins = ["http://localhost", "http://onysakura.xyz"])
+@CrossOrigin(origins = ["https://onysakura.xyz"])
 @RequestMapping("/test")
 class TestController {
 
